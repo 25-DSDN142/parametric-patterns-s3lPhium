a6b161e24f588dc674691796e6486e2ee0a0dc45
Cameron Wilkinson <22cameron.w@gmail.com>
2025-07-29 12:18:30 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,7 +1,7 @@
 //your parameter variables go here!
 
 //base values = 80
-let rect_width  = 40;
+let rect_width  = 80;
 let rect_height = 80;
 
 //base values = 80
@@ -9,16 +9,20 @@ let tri_width = 80;
 let tri_height = 80;
 
 //base values = 20
-let circle_height = 20;
-let circle_width = 20;
+let circle_height = 10;
+let circle_width = 10;
+
+//base values = 50
+let circle_positionX = 50;
+let circle_positionY = 50;
 
 
 
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  //pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
@@ -35,23 +39,24 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
-whiteDots();
+
 yellowRect(10, 10);
 greyRect(110, 110 );
 pinkRect(10, 110);
 blueRect(110, 10);
+whiteLines();
 
 }
 
 function yellowRect(x, y) {
 strokeWeight(2);
 strokeWeight(1);
-stroke(222, 215, 20);
+stroke(255, 227, 84);
 noFill()
 rect(x ,y, rect_width , rect_height); 
-fill(222, 215, 20)
+fill(255, 227, 84)
 rect(x, y, rect_width / 2 , rect_height)
-hatchRect(x, y, rect_width / 2, rect_height, 3);
+hatchRect(x, y, rect_width / 2, rect_height, 10);
 }
 
 function greyRect(x, y) {
@@ -61,43 +66,67 @@ stroke(199, 191, 199)
 rect(x ,y, rect_width , rect_height); 
 fill(199, 191, 199);
 rect(x ,y, rect_width / 2 , rect_height);
-hatchRect(x, y, rect_width / 2, rect_height, 3);
+hatchRect(x, y, rect_width / 2, rect_height, 10);
 
 }
 
 function pinkRect(x, y) {
 noFill()
 strokeWeight(1);
-stroke(219, 143, 217)
+stroke(226, 112, 164)
 rect(x ,y, rect_width, rect_height);
 noStroke();
-fill(219, 143, 217);
+fill(226, 112, 164);
 triangle(x + tri_width, y + tri_height, x + tri_width, y, x, y + tri_height);
+hatchTriPink(x, y, tri_width, tri_height, 10)
+
 
 }
 
 function blueRect(x, y) {
 strokeWeight(1);
 noFill()
-stroke(23, 68, 145)
+stroke(74, 139, 235)
 rect(x ,y, rect_width, rect_height);
 noStroke()
-fill(23, 68, 145);
+fill(74, 139, 235);
 triangle(x, y, x, y + tri_height, x + tri_width, y);
+hatchTriBlue(x, y, tri_width, tri_height, 10)
+
 }
 
-function whiteDots() {
-  // return();
-  if (rect_width <= 60)
-    ellipse(55, 40, circle_height, circle_height);
-    ellipse(155, 140, circle_width, circle_height);
+function whiteLines() {
+  noFill();
+  stroke("white")
+  beginShape()
 
-  }
+  
+
+  endShape()
+}
+
 
 function hatchRect(x, y, w, h, spacing = 0) {
   stroke('white');
   for (let i = 0; i < w; i += spacing) {
     line(x + i, y, x + i, y + h);
- }
+  }
+}
+
+function hatchTriBlue(x, y, w, h, spacing = 0) {
+  stroke('white');
+  for (let i = 0; i <= h; i += spacing) {
+    let lineLength = map(i, 0, h, w, 0);
+    line(x, y + i, x + lineLength, y + i);
+  }
+
+}
+
+function hatchTriPink(x, y, w, h, spacing = 5) {
+  stroke('white');
+  for (let i = 0; i <= h; i += spacing) {
+    let lineLength = map(i, 0, h, 0, w);
+    line(x + w - lineLength, y + i, x + w, y + i);
+  }
 
 }
